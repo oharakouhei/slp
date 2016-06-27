e4e0cce6c98f41e59e6a0ac85031384b3b0e2f32
KoheiOhara <gofue0723@gmail.com>
2016-06-27 17:50:40 +0900

    [modify] 動作確認済

--- edist.py
+++ edist.py
@@ -2,11 +2,25 @@
 import sys
 import numpy as np
 
-def MinimumEditDistance(object):
+class MinimumEditDistance(object):
     def __init__(self):
+        # Levenshtein distance
+        self.NO_COST = 0
         self.INS_COST = 1
         self.DEL_COST = 1
         self.SUBST_COST = 2
+
+    def _ins_cost(self):
+        return self.INS_COST
+
+    def _subst_cost(self, src_char, tgt_char):
+        if src_char == tgt_char:
+            return self.NO_COST
+        else:
+            return self.SUBST_COST
+
+    def _del_cost(self):
+        return self.DEL_COST
 
     def calc(self, source, target):
         '''
@@ -18,24 +32,29 @@ def MinimumEditDistance(object):
         distance[0] = range(len(target)+1)
         distance[:,0] = range(len(source)+1)
 
-        for tgt_id, tgt_char in enumerate(target):
-            for src_id, src_char in enumerate(source):
-                distance[src_id+1, tgt_id+1] = min(
-                    distance[src_id, tgt_id+1]+self.INS_COST,
-                    distance[src_id, tgt_id]+self.SUBST_COST,
-                    distance[src_id+1, tgt_id]+self.DEL_COST,
+        # update from distance[1,1] to distance[len(source), len(target)]
+        for i, src_char in enumerate(source):
+            for j, tgt_char in enumerate(target):
+                src_idx = i+1
+                tgt_idx = j+1
+                distance[src_idx, tgt_idx] = min(
+                    distance[src_idx-1, tgt_idx]+self._ins_cost(),
+                    distance[src_idx-1, tgt_idx-1]+self._subst_cost(src_char, tgt_char),
+                    distance[src_idx, tgt_idx-1]+self.DEL_COST,
                 )
+        return distance[len(source), len(target)]
 
-        return distance[len(source)+1, len(target)+1]
 
 if __name__ == "__main__":
     print("Please input 2 words.")
-    input_words = input("> ")
-    arr = input_words.split()
+    while(True):
+        input_words = input("> ")
+        arr = input_words.split()
 
-    if len(arr) != 2:
-        sys.stderr.write("It's not 2 words!\n")
-        sys.exit(1)
+        if len(arr) == 2:
+            break
+        else:
+            sys.stderr.write("It's not 2 words!\n")
 
     source_word = arr[0]
     target_word = arr[1]
