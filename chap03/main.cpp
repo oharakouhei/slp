9b589f3d92dcdfac4f9977673000e4c8fe7f35dc
KoheiOhara <gofue0723@gmail.com>
2016-06-29 14:38:49 +0900

    [modify] main.cppを微修正

--- chap03/main.cpp
+++ chap03/main.cpp
@@ -6,10 +6,10 @@ int main(int argc, char** argv)
     if (argc < 3) {
         std::cout << "Usage: " << argv[0] << " training_data_file test_data_file N(default 2)" << std::endl;
     }
-    std::string train_data_file = "/Users/kohei/Desktop/college/tkl/slp_meeting/NLP/n_gram/ishiwatari/data/mini2012.txt";
-    std::string test_data_file = "/Users/kohei/Desktop/college/tkl/slp_meeting/NLP/n_gram/ishiwatari/data/mini2013.txt";
+    std::string train_data_file = argv[1];
+    std::string test_data_file = argv[2];
     // TODO: N == 1に対応
-    int N = argc < 3 ? 3 : std::atoi(argv[3]);
+    int N = argc < 3 ? 2 : std::atoi(argv[3]);
 
     NGram ngram(N);
     ngram.train(train_data_file);
