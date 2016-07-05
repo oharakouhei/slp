766e61c25b8f53edcc49df36d60bfef9561464e8
KoheiOhara <gofue0723@gmail.com>
2016-07-05 22:46:01 +0900

    [add] sentence2ngram and common.h

--- chap03/cdr2sentence.cpp
+++ chap03/cdr2sentence.cpp
@@ -1,35 +1,34 @@
 //
-// 形態素解析結果のcdrファイルを、文に変換するスクリプト
+// 形態素解析結果のcdrファイルを、
+// 各単語をスペースで、各文を改行で区切る文に変換するスクリプト
 //
-#include <iostream>
-#include <fstream>
-
-std::string SENTENCE_END_SYMBOL = "EOS";
+#include "common.h"
 
 int main(int argc, char** argv)
 {
-    if (argc < 2) {
-        std::cerr << "Usage: " << argv[0] << " cdr_file" << std::endl;
-        std::exit(EXIT_FAILURE);
-    }
-    std::string cdr_file = argv[1];
+//    if (argc < 2) {
+//        std::cerr << "Usage: " << argv[0] << " cdr_file" << std::endl;
+//        std::exit(EXIT_FAILURE);
+//    }
+    std::string cdr_file = "/Users/kohei/Desktop/college/tkl/slp_meeting/slp_practices/chap03/data/mini2013.txt";
+//    std::string cdr_file = argv[1];
     std::ifstream input(cdr_file);
     if (!input) {
         std::cerr << "cannot find cdr file." << std::endl;
         std::exit(EXIT_FAILURE);
     }
     std::string line;
-    std::string delimiter = "\t";
+
     while (std::getline(input, line))
     {
-        std::string surface = line.substr(0, line.find(delimiter));
+        std::string surface = line.substr(0, line.find(CDR_DELIMITER));
         if (surface == SENTENCE_END_SYMBOL)
         {
             std::cout << std::endl;
         }
         else
         {
-            std::cout << surface << " ";
+            std::cout << surface << SENTENCE_DELIMITER;
         }
     }
 }
---
+++ chap03/common.h
@@ -0,0 +1,18 @@
+//
+// Created by OharaKohei on 2016/07/05.
+//
+
+#ifndef CHAP03_COMMON_H
+#define CHAP03_COMMON_H
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+std::string SENTENCE_END_SYMBOL = "EOS";
+std::string CDR_DELIMITER = "\t";
+char SENTENCE_DELIMITER = ' ';
+std::string NGRAM_START_SYMBOL = "<s>";
+std::string NGRAM_END_SYMBOL = "</s>";
+
+#endif //CHAP03_COMMON_H
