478f7465eb666a5803727c2a94fcf11488b1b606
KoheiOhara <gofue0723@gmail.com>
2016-07-17 15:53:11 +0900

    [clean] ファイル名修正．実行例をファイル冒頭に追加．

--- hmm/train2tagseq.cpp
+++ hmm/train2tagseq.cpp
@@ -1,7 +1,15 @@
 //
 // train.txt（形式は以下）からタグの並びを抽出するスクリプト
 //
-// 入力 (train.txt)
+////////////////////// 実行例 ////////////////////////////
+// $ cat train.txt | ./train2tagseq | head -5
+// NN IN DT NN VBZ RB VBN TO VB DT JJ NN IN NN NNS IN NNP , JJ IN NN NN , VB TO VB DT JJ NN IN NNP CC NNP POS JJ NNS .
+// NNP IN DT NNP NNP NNP POS VBN NN TO DT NN JJ NN VBZ VBN TO VB DT NN IN NN IN DT JJ NN .
+// CC NNS VBP VBG NN IN NN VBZ VBN VBN IN DT NN POS NN TO VB DT JJ NN NNS IN PRP$ NNP NNP NN JJ NNP .
+// DT VBZ VBN DT NN IN DT NN VBG VBN TO VB NN NNS TO CD NN IN PRP$ JJ CD NN NN TO VB DT NN , NNS CC JJ NN NN NNS VBP .
+// `` DT NNS IN NN IN DT JJ NN NN VBP RB RB IN DT JJ NN , '' VBD NNP NNP , JJ NNP NN IN NNP NNP NNP .
+//
+//////////////////// 入力 (train.txt) /////////////////////
 // He PRP B-NP
 // is VBZ B-VP
 // (He is から始まる一文の他の単語のタグ等の情報)
@@ -9,7 +17,7 @@
 // . . O
 // (改行で文の区切り)
 //
-// 出力例 (一文のタグの並びを一列に)
+//////////////// 出力例 (一文のタグの並びを一列に) ///////////////
 // PRP VBZ ... NNS .
 //
 //
